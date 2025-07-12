fae51cb28b3f2fa09d8f56aa1b28dba07fd9221d
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-07-12 12:24:37 +0300

    created agent, prompt and tools

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,4 +1,5 @@
 import Sandbox from '@e2b/code-interpreter';
+import { AgentResult, TextMessage } from '@inngest/agent-kit';
 
 export async function getSandbox(sandboxId: string) {
   const sandbox = await Sandbox.connect(sandboxId);
@@ -6,4 +7,16 @@ export async function getSandbox(sandboxId: string) {
     throw new Error(`Sandbox with ID ${sandboxId} not found`);
   }
   return sandbox;
+}
+
+export function lastAssistantTextMessageContent(result: AgentResult) {
+  const lastAssistantTextMessageIndex = result.output.findLastIndex(message => message.role === 'assistant');
+
+  const message = result.output[lastAssistantTextMessageIndex] as TextMessage | undefined;
+
+  return message?.content
+    ? typeof message.content === 'string'
+      ? message.content
+      : message.content.map(content => content.text).join('')
+    : undefined;
 }
