7a51a6eb00bad99d740f2532acbbf4d313077804
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-07-14 17:54:48 +0300

    renamed function and added message persistance to db

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,7 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter } from '../init';
+import { messageRouter } from '@/modules/messages/server/procedures';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string()
-      })
-    )
-    .query(opts => {
-      return {
-        greeting: `hello ${opts.input.text}`
-      };
-    })
+  messages: messageRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
